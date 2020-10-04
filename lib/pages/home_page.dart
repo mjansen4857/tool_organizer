d01a9c892439f3c348962a144a656017a60cc22e
Michael Jansen <mjansen4857@gmail.com>
2020-10-04 00:51:56 -0400

    add basic page scaffolding

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final String username;
+
+  HomePage({this.username});
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/pages/root_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import 'login_page.dart';
+
+class RootPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  bool loggedIn = false;
+  String username;
+
+  @override
+  void initState() {
+    super.initState();
+    loggedIn = false;
+    username = null;
+  }
+
+  void loginCallback(String username) {
+    loggedIn = true;
+    this.username = username;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!loggedIn) {
+      return LoginPage(loginCallback: loginCallback);
+    } else {
+      return HomePage(username: username);
+    }
+  }
+}
